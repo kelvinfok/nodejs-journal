39a9a41997639ab5b12540dacfb2ef08ffc039d4
kelvinfok <punkd1@gmail.com>
2018-12-17 17:42:06 +0800

    add delete feature

--- app.js
+++ app.js
@@ -2,24 +2,47 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const firebase = require("firebase");
+const admin = require('firebase-admin');
+const serviceAccount = require("./config/serviceAccountKey.json");
+const textbank = require(__dirname + "/textbank.js");
 
-const homeStartingContent = "some home content";
-const aboutContent = "some about content";
-const contactContent = "some contact content";
+var PORT = process.env.PORT || 3000;
+
+const homeStartingContent = textbank.headerText();
+const aboutContent = textbank.aboutText();
+const contactContent = textbank.contactText();
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://whatsappclone-d467c.firebaseio.com"
+});
 
-let posts = [];
-
+const db = admin.firestore();
+const postsCollection = db.collection('posts');
 
 app.get("/", function(req, res) {
-    res.render("home", { startingContent: homeStartingContent, posts: posts });
+    let posts = [];
+    postsCollection.get()
+    .then(function(snapshot) {
+        snapshot.forEach(function(snapshot) {
+            var post = {
+                title: snapshot.data().title,
+                content: snapshot.data().content,
+                id: snapshot.id
+            }
+            posts.push(post);
+        })
+        res.render("home", { startingContent: homeStartingContent, posts: posts });
+    }).catch(function(error) {
+        console.log('Error getting documents', error);
+    })
 });
 
 app.get("/about", function(req, res) {
@@ -35,15 +58,27 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-
-    const post = {
+    postsCollection.add({
         title: req.body.postTitle,
         content: req.body.postBody
-    };
-
-    posts.push(post);
-    res.redirect("/");
+    }).then(function(ref) {
+        console.log('Added document with ID: ', ref.id);
+        res.redirect("/");
+    }).catch(function(error) {
+        console.log("error: " + error);
+    })
 });
+
+app.post("/delete", function(req, res) {
+    const postId = req.body.postId;
+    postsCollection.doc(postId).delete()
+    .then(function(value) {
+        console.log(value);
+        res.redirect("/");
+    }).catch(function(error) {
+        console.log("error: " + error);
+    })
+})
 
 app.get("/post/:postName", function(req, res) {
     
@@ -74,6 +109,6 @@ app.get("/post/:postName", function(req, res) {
     console.log(req.params.postName);
 })
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(PORT, function() {
     console.log("Listening on port 3000");
 });
---
+++ textbank.js
@@ -0,0 +1,12 @@
+
+module.exports.headerText = function() {
+    return "Welcome to my journal built with NodeJS. Feel free to create a post and play around with the navigation."
+}
+
+module.exports.aboutText = function() {
+    return "The emergence and growth of blogs in the late 1990s coincided with the advent of web publishing tools that facilitated the posting of content by non-technical users who did not have much experience with HTML or computer programming. Previously, a knowledge of such technologies as HTML and File Transfer Protocol had been required to publish content on the Web, and early Web users therefore tended to be hackers and computer enthusiasts. In the 2010s, the majority are interactive Web 2.0 websites, allowing visitors to leave online comments, and it is this interactivity that distinguishes them from other static websites.[2] In that sense, blogging can be seen as a form of social networking service. Indeed, bloggers do not only produce content to post on their blogs, but also often build social relations with their readers and other bloggers.[3] However, there are high-readership blogs which do not allow comments."
+}
+
+module.exports.contactText = function() {
+    return "Lorem ipsum dolor sit amet, ad mea ludus utamur, ne vix timeam quaestio. Justo admodum tincidunt nam ne. Est ei modo debitis similique, eos et nulla partiendo, autem velit denique vim in. Ei audiam vituperata per, nostro sanctus conceptam at est. Ei efficiendi contentiones conclusionemque sea, ea nec aperiri deserunt. Nulla dolorem at sea. Per in semper impedit volumus, ea eam feugiat scripserit. Eum cetero persius omittantur an, eu legimus quaestio per. Mundi albucius constituam vix at. Amet mentitum lucilius ad his, assum cotidieque ad has, ius offendit delectus neglegentur id."
+}
